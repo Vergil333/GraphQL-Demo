ff8ad97c0e229d3220dfa871810e8b7d8a651e93
Martin Machava <Vergil333@gmail.com>
2020-07-01 14:20:16 +0200

    use only node-fetch

--- src/coingecko-client.ts
+++ src/coingecko-client.ts
@@ -1,9 +1,6 @@
 import fetch from 'node-fetch'
-const CoinGeckoApi = require('coingecko-api')
 
 const allCoinsUrl = 'https://api.coingecko.com/api/v3/coins/list'
-
-const coinGeckoClient = new CoinGeckoApi()
 
 class Coin {
     constructor(public id: string,
@@ -13,13 +10,5 @@ class Coin {
 
 export async function getAllCoins(): Promise<Coin[]> {
     const response = await fetch(allCoinsUrl)
-    const json = await response.json()
-    return json
-}
-
-export async function getCoinsFromGCApi(): Promise<Coin[]> {
-    let allCoins = await coinGeckoClient.coins.all(['params.page=50'])
-    console.log(allCoins)
-    let coins = allCoins.data.map((coin: { id: string; symbol: string; name: string }) => new Coin(coin.id, coin.symbol, coin.name))
-    return await coins
+    return await response.json()
 }
