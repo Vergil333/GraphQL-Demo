9815e6507eeb627a36e42d2242942c533c0327c5
Martin Machava <Vergil333@gmail.com>
2020-07-08 22:11:31 +0200

    separate logic into imported services

--- src/coingecko-client.ts
+++ src/coingecko-client.ts
@@ -9,19 +9,22 @@ import fetch from 'node-fetch'
 
 const allCoinsUrl = 'https://api.coingecko.com/api/v3/coins/list'
 
-export interface Coin {
+export interface CGCoin {
     id: string,
     name: string | null,
     symbol: string
 }
 
-async function getAllCoins(): Promise<Coin[]> {
-    const response = await fetch(allCoinsUrl)
-    return await response.json()
+async function getAllCoins(): Promise<CGCoin[]> {
+    const coins = await fetch(allCoinsUrl)
+    const coinsJson = await coins.json()
+    console.log(`${coinsJson.length} coins have been successfully loaded from CoinGecko.`)
+
+    return coinsJson
 }
 
-const gcClient = {
+const cgClient = {
     getAllCoins,
 }
 
-export default gcClient
+export default cgClient
